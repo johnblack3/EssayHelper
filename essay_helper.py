aafe0627bc1f28d82b0d797f0a0b27d4b37e6a69
John Black <johnblack7497@gmail.com>
2022-11-14 17:31:15 -0500

    Fix spacing and comments

--- essay_helper.py
+++ essay_helper.py
@@ -1,21 +1,25 @@
-'''
+"""
 EssayHelper is a tool used to analyze and provide feedback on a written essay.
 Created by John Black
 11/01/2022
-'''
+"""
 
-from collections import Counter    # counter to sort arrays by number of occurances
-import os.path    # check for bad_phrases.txt and cliches.txt
+from collections import Counter
+import os.path
 
 class Essay:
-    
-    def __init__(self, essay):
+    """
+    This class creates Essay objects and holds methods to analyze the Essay Objects
+    """
+    def __init__(self, essay, num_of_spaces_after_period=1):
         self.essay = self.format_essay(essay)
+        self.num_of_spaces_after_period = num_of_spaces_after_period
 
     def format_essay(self, essay):
         """Format string for ease of use."""
         e = essay.strip()
         e = e.replace("  ", " ")
+        e = e.replace("\n", " ")
         return e
     
     def num_of_words(self):
@@ -30,9 +34,14 @@ class Essay:
         """Find the number of sentences. Not finished."""
         not_end_of_sentence = ["Mr", "Mrs", "Inc"] # words with trailing periods which do not denote the end of a sentece   # NEEDS WORK - add more words, fix for quotes
         num_of_sentences = 0
+        position = 0
         for c in self.essay:
-            if c == "." or c == "!" or c == "?":
-                num_of_sentences += 1
+            if c in [".", "?", "!"]:
+                if position == len(self.essay) - 1:
+                    break
+                elif self.essay[position + 1] == " ":
+                    num_of_sentences += 1
+            position += 1
         print("Sentences:", num_of_sentences)
 
     def num_of_chars(self):
@@ -48,40 +57,40 @@ class Essay:
     def start_of_sentence(self):
         """Find the number of times each word starts a sentence."""
         first_words = []
-        first_words.append(self.essay[0: self.essay.find(" ")]) # add first word in string (position 0 to first space)
+        first_words.append(self.essay[0: self.essay.find(" ")])
         position = 0
         not_end_of_sentence = ["Mr", "Mrs", "Inc"] # words with trailing periods which do not denote the end of a sentece   # NEEDS WORK - add more words, fix for quotes
-
         for c in self.essay:
-            if c == ".":
+            if c in [".", "?", "!"]:
                 if position == len(self.essay) - 1:
                     break
                 elif self.essay[self.essay.rfind(' ', 0, position)+1: position] in not_end_of_sentence: # don't count abbreviations as end of sentence    # NEEDS WORK
                     pass
-                else: # find the first space (or comma) after the space directly after the period
-                    start = position + 2
-                    end = self.essay.find(" ", position + 2)
-                    if self.essay.find(",", position + 2) < end and self.essay.find(",", position + 2) > 0: # if a comma comes before the first space, use the comma as the end of the word    # Efficieny?
-                            end = self.essay.find(",", position + 2)
+                elif self.essay[position + 1] != " ":
+                    pass
+                else:
+                    start = position + self.num_of_spaces_after_period + 1
+                    end = self.essay.find(" ", position + self.num_of_spaces_after_period + 1)
+                    if (self.essay.find(",", position + self.num_of_spaces_after_period + 1) < end and
+                            self.essay.find(",", position + self.num_of_spaces_after_period + 1) > 0):
+                        end = self.essay.find(",", position + self.num_of_spaces_after_period + 1)
                     first_words.append(self.essay[start:end])
-                
             position += 1
             
         # sort the items in the first_words list by the number of occurances
-        sorted_first_words = [item for items, c in Counter(first_words).most_common() for item in [items] * c]
+        sorted_first_words = [item for items,
+            c in Counter(first_words).most_common() for item in [items] * c]
         # remove duplicate words from list (sorted)
         res = []
         for i in sorted_first_words:
             if i not in res:
                 res.append(i)
         
-        # find the number of occurances of each word
         words_and_occurs = {}
-        
         occur = Counter(sorted_first_words)
         for item in res:
-            words_and_occurs[item] = occur[item] # add items to dictionary
-            print('"{}" starts {} sentences'.format(item, occur[item])) # print how many times each word occurs
+            words_and_occurs[item] = occur[item]
+            print('"{}" starts {} sentences'.format(item, occur[item]))
 
     def word_occurances(self):
         """Find the number of times each word occurs."""
@@ -92,7 +101,7 @@ class Essay:
             if c == " ":
                 if position == len(self.essay) - 1:
                     break
-                else: # find the first space after the space directly after the period
+                else:
                     start = position + 1
                     end = self.essay.find(" ", position + 2)
                     words.append(self.essay[start:end].lower())
@@ -100,19 +109,19 @@ class Essay:
             position += 1
             
         # sort the items in the words list by the number of occurances
-        sorted_words = [item for items, c in Counter(words).most_common() for item in [items] * c]
+        sorted_words = [item for items,
+            c in Counter(words).most_common() for item in [items] * c]
         # remove duplicate words from list (sorted)
         res = []
         for i in sorted_words:
             if i not in res:
                 res.append(i)
         
-        # find the number of occurances of each word
-        words_and_occurs = {} # dictionary to store words and number of occurances
+        words_and_occurs = {}
         occur = Counter(sorted_words)
         for item in res:
-            words_and_occurs[item] = occur[item] # add items to dictionary
-            print('"{}" is used {} times'.format(item, occur[item])) # print how many times each word occurs
+            words_and_occurs[item] = occur[item]
+            print('"{}" is used {} times'.format(item, occur[item]))
     
     def bad_phrases(self):
         """Find terms, phrases, and cliches that should be removed."""
@@ -131,7 +140,6 @@ class Essay:
                 for x in bad_phrases_used: print('"' + x.capitalize() + '"')
         else:
             print("There is no list of terms/phrases to be checked for.")
-
 
         if os.path.exists('cliches.txt'):
             with open('cliches.txt') as f:
